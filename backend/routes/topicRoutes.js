5e084ef95b403e864dd29d9e2b48d7b177416751
Diego Ragosta <di154020@ucf.edu>
2026-06-23 13:27:46 -0400

    Add topic routes for CRUD operations

--- backend/routes/topicRoutes.js
+++ backend/routes/topicRoutes.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const router = express.Router();
 
+const {
+    getTopics,
+    gettopicBySlug,
+    createTopic,
+} = require("../controllers/topicController");
+
+router.get("/", getTopics);
+router.get("/:slug", gettopicBySlug);
+router.post("/", createTopic);
+
+module.exports = router;
