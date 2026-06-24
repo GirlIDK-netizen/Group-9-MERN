c657a1ce3236b35341ccd1118aa6675039a945b7
Diego Ragosta <di154020@ucf.edu>
2026-06-24 12:59:53 -0400

    Add quiz routes for getting and creating questions

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const router = express.Router();
 
+const {
+    getQuestionsByTopic,
+    createQuestion
+} = require("../controllers/questionController");
+
+router.get("/:topicSlug", getQuestionsByTopic);
+router.post("/", createQuestion);
+
+module.exports = router;
