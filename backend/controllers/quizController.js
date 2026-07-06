42be97c41ceac47d27004fb998a65a14b1826942
Diego Ragosta <di154020@ucf.edu>
2026-07-06 13:18:34 -0400

    Refactor getQuestionByTopic to getQuestionsByTopic

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -1,28 +1,28 @@
-//controllers/quizController.js
-
 const QuizQuestion = require("../models/QuizQuestion");
 
-const getQuestionByTopic = async (req, res) => {
-    try {
-        const questions = await QuizQuestion.find({
-            topicSlug: req.params.topicSlug,
-        });
-        res.json(questions);
-    } catch (error) {
-        res.status(500).json({ message: "Error fetching questions", error });
-    }
+const getQuestionsByTopic = async (req, res) => {
+  try {
+    const questions = await QuizQuestion.find(
+      { topicSlug: req.params.topicSlug },
+      "-correctAnswer -explanation"
+    );
+
+    res.json(questions);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching questions", error });
+  }
 };
-    //for creating questions with POST /api/quizzes
+
 const createQuestion = async (req, res) => {
-    try{
-        const question = await QuizQuestion.create(req.body);
-        res.status(201).json(question);
-    } catch(error) {
-        res.status(400).json({ message: error.message});
-    }
+  try {
+    const question = await QuizQuestion.create(req.body);
+    res.status(201).json(question);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 module.exports = {
-    getQuestionsByTopic,
-    createQuestion
+  getQuestionsByTopic,
+  createQuestion
 };
